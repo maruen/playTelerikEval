9a2d13eded09dec97b66cd55b48428d6c5e50049
Maruen Mehana <maruen@gmail.com>
2014-11-12 20:14:27 -0200

    Refactorings

---
+++ public/javascripts/menuItems1.js
@@ -0,0 +1,49 @@
+	            
+$("#MenuItems1").kendoPanelBar({
+    dataSource: [
+        {
+            text: "DashBoards"					, imageUrl:'../assets/images/database.png',
+            items: [
+                    { text: "Produtos"			, imageUrl:'../assets/images/star.png' },
+                    { text: "Photo Galleries"	, imageUrl:'../assets/images/star.png' },
+                    { text: "Videos Records"	, imageUrl:'../assets/images/star.png' },
+                    { text: "Radio Records"		, imageUrl:'../assets/images/star.png' }
+                ]
+        },
+        
+        {
+            text: "Cadastros"					, imageUrl:'../assets/images/database.png',
+            items: [
+                { text: "Produtos"				, imageUrl:'../assets/images/star.png' },
+                { text: "Photo Galleries"		, imageUrl:'../assets/images/star.png' },
+                { text: "Videos Records"		, imageUrl:'../assets/images/star.png' },
+                { text: "Radio Records"			, imageUrl:'../assets/images/star.png' }
+            ]
+        },
+        
+        {
+            text: "Processos"					, imageUrl:'../assets/images/database.png',
+            items: [
+                { text: "Top News"				, imageUrl:'../assets/images/star.png' },
+                { text: "Photo Galleries"		, imageUrl:'../assets/images/star.png' }
+            ]
+        },
+        
+        {
+            text: "Relatórios"					, imageUrl:'../assets/images/database.png',
+            items: [
+                { text: "Photo Galleries"		, imageUrl:'../assets/images/star.png' },
+                { text: "Videos Records"		, imageUrl:'../assets/images/star.png' }
+            ]
+        },
+        
+        {
+            text: "Análise Gerencial"			, imageUrl:'../assets/images/database.png',
+            items: [
+                { text: "Photo Galleries"		, imageUrl:'../assets/images/star.png' },
+                { text: "Videos Records"		, imageUrl:'../assets/images/star.png' }
+            ]
+        }
+    ]
+});
+
