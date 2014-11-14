146288e2b8b8490fe349f4b78da640531882813f
Maruen Mehana <maruen@gmail.com>
2014-11-14 12:04:38 -0200

    Refactorings

--- app/controllers/MainController.java
+++ app/controllers/MainController.java
@@ -7,46 +7,37 @@ import play.libs.Json;
 import play.libs.Jsonp;
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.datatable1;
-import views.html.datatable2;
+import views.html.*;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
 
-public class TelerikController extends Controller {
+public class MainController extends Controller {
     
-    public static Result datatable1(int page,
-    								String sortBy,
-    								String order,
-    								String filter) {
-        
-    	return ok(datatable1.render(Computer.page(page,
-    											  10,
-    											  sortBy,
-    											  order,
-    											  filter
-    											  ),
-    								sortBy,
-    								order,
-    								filter)
-    			);
+    
+	public static Result GO_HOME = redirect(
+    		routes.MainController.page1(0, "name", "asc", "")
+    );
+	
+    public static Result index() {
+    	return GO_HOME;
+    }
+	
+	public static Result datatable1(int page,String sortBy,	String order,String filter) {
+    	return ok(datatable1.render(Computer.page(page,10, sortBy, order, filter),sortBy,order,filter));
     }
     
-    public static Result datatable2(int page,
-    								String sortBy,
-    								String order,
-    								String filter) {
+    public static Result page1(int page, String sortBy, String order, String filter) {
+    	return ok(page1.render(Computer.page(page,10,sortBy,order,filter),sortBy,order,filter));    	
+    }
+    
+    public static Result page2(int page,String sortBy,String order, String filter) {
+    	return ok(page2.render(Computer.page(page,10,sortBy,order,filter),sortBy,order,filter));
+    }
+    
+    public static Result telerikGrid(int page,String sortBy,String order,String filter) {
         
-    	return ok(datatable2.render(Computer.page(page,
-				  								  10,
-				  								  sortBy,
-				  								  order,
-				  								  filter
-				  								  ),
-				  					sortBy,
-				  					order,
-				  					filter)
-    			);
+    	return ok(telerikGrid.render(Computer.page(page,10,sortBy, order,filter),sortBy,order,filter));
     }
     
     public static Result computers(String callback) {
@@ -60,8 +51,6 @@ public class TelerikController extends Controller {
         JsonNode  	   json 		= Json.toJson(computers);
         return ok(Json.toJson(json));
     }
-    
-    
     
     public static Result update(String callback, Computer computer) {
         computer.update();
