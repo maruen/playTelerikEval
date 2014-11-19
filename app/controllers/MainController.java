a2c5f1dbdda2b9e4012556e2adfe4df84b9da415
Maruen Mehana <maruen@gmail.com>
2014-11-19 17:11:15 -0200

    Refactorings

--- app/controllers/MainController.java
+++ app/controllers/MainController.java
@@ -8,13 +8,14 @@ import play.libs.Json;
 import play.libs.Jsonp;
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.components.*;
-import views.html.forms.*;
-import views.html.pages.*;
-
+import views.html.components.datatable1;
+import views.html.components.datatable2;
+import views.html.forms.createUser;
+import views.html.forms.newRegisterForm;
+import views.html.pages.page1;
+import views.html.pages.page2;
 
 import com.fasterxml.jackson.databind.JsonNode;
-
 
 public class MainController extends Controller {
     
@@ -23,7 +24,7 @@ public class MainController extends Controller {
     }
 	
 	public static Result GO_HOME = redirect(
-    		routes.MainController.page1()
+    	routes.MainController.page1()
     );
 	
 	public static Result createUser() {
@@ -37,17 +38,17 @@ public class MainController extends Controller {
 	public static Result datatable1() {
     	return ok(datatable1.render());
     }
-    
+	
+	public static Result datatable2() {
+    	return ok(datatable2.render( new Computer()));
+    }
+	
     public static Result page1() {
     	return ok(page1.render());    	
     }
     
     public static Result page2() {
     	return ok(page2.render());
-    }
-    
-    public static Result telerikGrid() {
-    	return ok(telerikGrid.render());
     }
     
     public static Result computers(String callback) {
---
+++ public/javascripts/app/components/datatable2.js
@@ -0,0 +1,88 @@
+
+var crudServiceBaseUrl = "http://localhost:9000",
+    dataSource = new kendo.data.DataSource({
+	    transport: {
+	        read:  {
+	            url: crudServiceBaseUrl + "/computers",
+	            dataType: "jsonp"
+	        },
+	        update: {
+	            url: crudServiceBaseUrl + "/computers/update",
+	            dataType: "jsonp"
+	        },
+	        destroy: {
+	            url: crudServiceBaseUrl + "/computers/delete",
+	            dataType: "jsonp"
+	        },
+	        create: {
+	            url: crudServiceBaseUrl + "/computers/create",
+	            dataType: "jsonp"
+	        },
+	        parameterMap: function(options, operation) {
+	            if (operation !== "read" && options.models) {
+	                return {models: kendo.stringify(options.models)};
+	            }
+	        }
+	    },
+	    batch: true,
+	    pageSize: 10,
+	    schema: {
+	        model: {
+	            id: "id",
+	            fields: {
+	                id: 		 	{ editable: false, nullable: true },
+	                name: 		 	{ validation: { required: true } },
+	                introduced: 	{ type: "string" },
+	                discontinued: 	{ type: "string" },
+	                company: 		{ type: "number"  }
+	            }
+	        }
+	    }
+    });
+
+
+$("#grid").kendoGrid({
+    dataSource: dataSource,
+    pageable: true,
+    sortable: true,
+    height: 490,
+    toolbar: [                          
+              	  "create",
+              	  {
+                      text: "Novo Registro",
+                      className: "newOrUpdateClass",
+                  },
+                  {
+                      text : "Exportar Excel", 
+                      name: "popup3", 
+                      iconClass: "k-icon k-add"
+                  },
+                  {
+                      text : "Exportar PDF", 
+                      name: "popup4", 
+                      iconClass: "k-icon k-add"
+                  },
+              ],
+    columns: [
+        		"name",
+        		{ field: "discontinued"			, 	title: "Discontinued"	, width: "120px" },
+        		{ field: "Company"				, 	title: "Company"		, width: "120px" },
+        		{ field: "introduced"			, 	title: "Data"			, width: "120px" },
+        		{ command: ["edit", "destroy"]	,   title: "&nbsp;"			, width: "200px" }
+        ],
+    
+ 	editable: "inline"
+     	
+});
+
+
+$(".newOrUpdateClass").click(function() {
+   $("#datatable").hide();
+   $("#edit").show();
+});
+
+$("#cancelButton").click(function() {
+	$("#datatable").show();
+    $("#edit").hide();
+});
+
